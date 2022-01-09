4801ee0cb95ee0f44d3a9936ef89520d7a9af31e
hhacker1999 <harsh.sadh313@gmail.com>
2022-01-09 12:18:50 +0530

    feat(movie_repository_implementation.dart): implement get movies method

--- lib/insfrastructure/repository/movie_repository_implementation.dart
+++ lib/insfrastructure/repository/movie_repository_implementation.dart
@@ -1,13 +1,32 @@
+import 'dart:convert';
+
+import 'package:torrenter/app/constants.dart';
 import 'package:torrenter/domain/entity/movie_entity.dart';
 import 'package:torrenter/domain/repository/movies_repository.dart';
 import 'package:http/http.dart' as http;
 
 class MovieRepositoryImplementation implements MoviesRepository {
+  int page = 1;
   final http.Client _client;
 
-  const MovieRepositoryImplementation(this._client);
+  MovieRepositoryImplementation(this._client);
   @override
-  Future<MovieEntity> getMovies() {
-    throw UnimplementedError();
+  Future<List<MovieEntity>> getMovies() async {
+    try {
+      final uri = AppConstants.ytsApiPage(page);
+      final res = await _client.get(Uri.parse(uri));
+      if (res.statusCode == 200) {
+        page++;
+        final Map data = jsonDecode(res.body);
+        final List movies = data["movies"];
+        final List<MovieEntity> movieEntities =
+            movies.map((a) => MovieEntity.fromJson(a)).toList();
+        return movieEntities;
+      } else {
+        throw "Bad connection";
+      }
+    } catch (_) {
+      rethrow;
+    }
   }
 }
