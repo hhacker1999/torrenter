f7c3f9a84516fb6684f5d00505b8594c0b07e700
hhacker1999 <harsh.sadh313@gmail.com>
2022-01-09 12:18:07 +0530

    fix(movie_entity.dart): complete movie entity instead of mockup value

--- lib/domain/entity/movie_entity.dart
+++ lib/domain/entity/movie_entity.dart
@@ -1,6 +1,124 @@
-class MovieEntity {
-  final String name;
-  final String image;
+import 'dart:convert';
+import 'torrent_entity.dart';
 
-  const MovieEntity({required this.name, required this.image});
+MovieEntity movieEntityFromJson(String str) =>
+    MovieEntity.fromJson(json.decode(str));
+
+String movieEntityToJson(MovieEntity data) => json.encode(data.toJson());
+
+class MovieEntity {
+  MovieEntity({
+    required this.id,
+    required this.url,
+    required this.imdbCode,
+    required this.title,
+    required this.titleEnglish,
+    required this.titleLong,
+    required this.slug,
+    required this.year,
+    required this.rating,
+    required this.runtime,
+    required this.genres,
+    required this.summary,
+    required this.descriptionFull,
+    required this.synopsis,
+    required this.ytTrailerCode,
+    required this.language,
+    required this.mpaRating,
+    required this.backgroundImage,
+    required this.backgroundImageOriginal,
+    required this.smallCoverImage,
+    required this.mediumCoverImage,
+    required this.largeCoverImage,
+    required this.state,
+    required this.torrents,
+    required this.dateUploaded,
+    required this.dateUploadedUnix,
+  });
+
+  final int id;
+  final String url;
+  final String imdbCode;
+  final String title;
+  final String titleEnglish;
+  final String titleLong;
+  final String slug;
+  final int year;
+  final int rating;
+  final int runtime;
+  final List<String> genres;
+  final String summary;
+  final String descriptionFull;
+  final String synopsis;
+  final String ytTrailerCode;
+  final String language;
+  final String mpaRating;
+  final String backgroundImage;
+  final String backgroundImageOriginal;
+  final String smallCoverImage;
+  final String mediumCoverImage;
+  final String largeCoverImage;
+  final String state;
+  final List<Torrent> torrents;
+  final DateTime dateUploaded;
+  final int dateUploadedUnix;
+
+  factory MovieEntity.fromJson(Map<String, dynamic> json) => MovieEntity(
+        id: json["id"],
+        url: json["url"],
+        imdbCode: json["imdb_code"],
+        title: json["title"],
+        titleEnglish: json["title_english"],
+        titleLong: json["title_long"],
+        slug: json["slug"],
+        year: json["year"],
+        rating: json["rating"],
+        runtime: json["runtime"],
+        genres: List<String>.from(json["genres"].map((x) => x)),
+        summary: json["summary"],
+        descriptionFull: json["description_full"],
+        synopsis: json["synopsis"],
+        ytTrailerCode: json["yt_trailer_code"],
+        language: json["language"],
+        mpaRating: json["mpa_rating"],
+        backgroundImage: json["background_image"],
+        backgroundImageOriginal: json["background_image_original"],
+        smallCoverImage: json["small_cover_image"],
+        mediumCoverImage: json["medium_cover_image"],
+        largeCoverImage: json["large_cover_image"],
+        state: json["state"],
+        torrents: List<Torrent>.from(
+            json["torrents"].map((x) => Torrent.fromJson(x))),
+        dateUploaded: DateTime.parse(json["date_uploaded"]),
+        dateUploadedUnix: json["date_uploaded_unix"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "url": url,
+        "imdb_code": imdbCode,
+        "title": title,
+        "title_english": titleEnglish,
+        "title_long": titleLong,
+        "slug": slug,
+        "year": year,
+        "rating": rating,
+        "runtime": runtime,
+        "genres": List<dynamic>.from(genres.map((x) => x)),
+        "summary": summary,
+        "description_full": descriptionFull,
+        "synopsis": synopsis,
+        "yt_trailer_code": ytTrailerCode,
+        "language": language,
+        "mpa_rating": mpaRating,
+        "background_image": backgroundImage,
+        "background_image_original": backgroundImageOriginal,
+        "small_cover_image": smallCoverImage,
+        "medium_cover_image": mediumCoverImage,
+        "large_cover_image": largeCoverImage,
+        "state": state,
+        "torrents": List<dynamic>.from(torrents.map((x) => x.toJson())),
+        "date_uploaded": dateUploaded.toIso8601String(),
+        "date_uploaded_unix": dateUploadedUnix,
+      };
 }
