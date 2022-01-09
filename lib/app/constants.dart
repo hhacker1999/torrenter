511aa794d0a05d3a9250157a5b3cdd98ff69347e
hhacker1999 <harsh.sadh313@gmail.com>
2022-01-09 15:28:46 +0530

    refactor(constants.dart): add white color constant

--- lib/app/constants.dart
+++ lib/app/constants.dart
@@ -1,8 +1,12 @@
+import 'package:flutter/material.dart';
+
 class AppConstants {
-  static const String ytsApi = "https://yts.mx/api/v2/list_movies.json";
+  static const String _ytsApi = "https://yts.mx/api/v2/list_movies.json";
   static String ytsApiPage(int page) {
     final String stringPage = page.toString();
-    final String url = ytsApi + "?page=" + stringPage;
+    final String url = _ytsApi + "?page=" + stringPage;
     return url;
   }
+
+  static Color get whiteGrey=> Colors.white.withOpacity(0.5);
 }
