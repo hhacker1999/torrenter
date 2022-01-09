8559f69b43a277b9ffa964bf8943e54644801653
hhacker1999 <harsh.sadh313@gmail.com>
2022-01-09 11:58:17 +0530

    feat(torrent_entity.dart): create a torrent entity class

---
+++ lib/domain/entity/torrent_entity.dart
@@ -0,0 +1,52 @@
+class Torrent {
+  Torrent({
+    required this.url,
+    required this.hash,
+    required this.quality,
+    required this.type,
+    required this.seeds,
+    required this.peers,
+    required this.size,
+    required this.sizeBytes,
+    required this.dateUploaded,
+    required this.dateUploadedUnix,
+  });
+
+  final String url;
+  final String hash;
+  final String quality;
+  final String type;
+  final int seeds;
+  final int peers;
+  final String size;
+  final int sizeBytes;
+  final DateTime dateUploaded;
+  final int dateUploadedUnix;
+
+  factory Torrent.fromJson(Map<String, dynamic> json) => Torrent(
+        url: json["url"],
+        hash: json["hash"],
+        quality: json["quality"],
+        type: json["type"],
+        seeds: json["seeds"],
+        peers: json["peers"],
+        size: json["size"],
+        sizeBytes: json["size_bytes"],
+        dateUploaded: DateTime.parse(json["date_uploaded"]),
+        dateUploadedUnix: json["date_uploaded_unix"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "url": url,
+        "hash": hash,
+        "quality": quality,
+        "type": type,
+        "seeds": seeds,
+        "peers": peers,
+        "size": size,
+        "size_bytes": sizeBytes,
+        "date_uploaded": dateUploaded.toIso8601String(),
+        "date_uploaded_unix": dateUploadedUnix,
+      };
+}
+
